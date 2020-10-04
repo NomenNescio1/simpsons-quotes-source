1a04e1bfe391919797951e53cf8ac7a922745397
nomennescio1 <daniel.rodriguez@wikot.com>
2020-10-04 16:30:08 -0500

    Updates

---
+++ precache-manifest.8b7c4bcdd1393610e5aa43e7ba56b7a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91dd630f6058414ad7b240f61856cbd9",
+    "url": "/quotes-release/index.html"
+  },
+  {
+    "revision": "ec5b45ed6bb08c5c24e0",
+    "url": "/quotes-release/static/css/main.dc7adbcd.chunk.css"
+  },
+  {
+    "revision": "3f6bd0d585a564f6d14a",
+    "url": "/quotes-release/static/js/2.5f10d441.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/quotes-release/static/js/2.5f10d441.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec5b45ed6bb08c5c24e0",
+    "url": "/quotes-release/static/js/main.a5f1a9e8.chunk.js"
+  },
+  {
+    "revision": "93419ae96271cbd51fdb",
+    "url": "/quotes-release/static/js/runtime-main.f57bed6f.js"
+  }
+]);
